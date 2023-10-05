930c4679c5d2aeb70fb7350c34567a5ed673733d
THOSSYN <mtafortunefinder@gmail.com>
2023-10-05 03:18:29 -0700

    Added Tasks 1 and 2 files

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# A script that sets up webservers for deployment
+
+if ! command nginx -v &> /dev/null; then
+	sudo apt-get update
+	sudo apt-get install nginx -y
+fi
+
+base_dir="/data/web_static/"
+releases_dir="$base_dir/releases/test"
+shared_dir="$base_dir/shared"
+rel_index="$releases_dir/test/index.html"
+nginx_cfgfile="/etc/nginx/sites-available/test_web"
+
+for dir in "$base_dir" "$releases_dir" "$shared_dir"; do
+	if [ ! -d "$dir" ]; then
+		sudo mkdir -p "$dir"
+	fi
+done
+
+if [ ! -f "$rel_index" ]; then
+	touch "$rel_index"
+	echo "Holberton School" | sudo tee "$rel_index" > /dev/null
+fi
+
+if [ -L "$base_dir/current" ]; then
+	sudo unlink "$base_dir/current"
+fi
+
+sudo ln -s "$releases_dir" "$base_dir/current"
+
+sudo chown -R "$USER:$USER" /data/
+
+echo "server {
+	listen 80;
+	server_name neo-ark.tech;
+	root $base_dir/current;
+	index index.html;
+
+	location /hbnb_static {
+		alias $base_dir/current;
+	}
+}" | sudo tee "$nginx_cfgfile" > /dev/null
+
+sudo ln -sf "$nginx_cfgfile" "/etc/nginx/sites-enabled/"
+
+sudo nginx -s reload
+
+exit 0
---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Compresses a folder into a zip file"""
+
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """Generates a compressed .tgz file from the web_static folder"""
+
+    # Get the current date and time
+    current_date = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    chek = local("if [ ! -d versions ]; then mkdir versions; fi", capture=True)
+    if chek.return_code != 0:
+        return None
+
+    archive_path = f'versions/web_static_{current_date}.tgz'
+
+    archive = local(f"tar -czvf {archive_path} web_static", capture=True)
+
+    if archive.return_code == 0:
+        return archive_path
+    else:
+        return None
