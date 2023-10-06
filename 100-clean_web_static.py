3e24fe7d8b22597ff537625c3fecadaa9362e76e
THOSSYN <mtafortunefinder@gmail.com>
2023-10-06 14:35:06 -0700

    Corrected: 100-clean_web_static.py

---
+++ 100-clean_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Deletes out-of-date archive"""
+
+from fabric.api import *
+from pathlib import Path
+import os
+
+env.hosts = ['54.237.100.5', '52.3.255.219']
+
+
+def do_clean(number=0):
+    """Function that will clean-up old archive"""
+    umber = 1 if number == 0 else int(number)
+
+    fileNames = [Path(i) for i in os.listdir("versions")]
+    for fullPath in fileNames[:-number]:
+        local(f"rm -r versions/{fullPath.name}")
+
+    with cd("/data/web_static/releases"):
+        fileNames = [i for i in run("ls").split()
+                     if i.startswith("web_static_")]
+        for fullPath in fileNames[:-number]:
+            run(f"rm -r {fullPath}")
