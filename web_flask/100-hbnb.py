aaabad435d6ea62fa4692aba1400f157c92307b4
THOSSYN <mtafortunefinder@gmail.com>
2023-10-22 14:58:25 -0700

    Added Advanced task

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -6,7 +6,7 @@ from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.city import City
-
+from models.place import Place
 
 app = Flask(__name__)
 
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,20 +14,32 @@ def remove_session(exception):
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
+"""@app.route('/states', strict_slashes=False)
 def states_list():
-    """Displays a list of state"""
+    Displays a list of state
     states = sorted(storage.all(State).values(), key=lambda state: state.name)
     return render_template('9-states.html', states=states)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_list_by_id(id):
-    """Displays a list of state"""
+    Displays a list of state
     for state in storage.all(State).values():
         if state.id == id:
             return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
+    return render_template('9-states.html')"""
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """Displays a list of state"""
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    return render_template('index.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id):
+    """Displays a list of state"""
+    states = sorted(storage.all(State).values(), key=lambda s: s.name)
+    return render_template('index.html', states=states, id=id)
 
 
 if __name__ == '__main__':
